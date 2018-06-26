59dd752c6f3c8ed9abe452ffe5319d9c577c9778
Alan-PC\Alan <yunjenchunmu@gmail.com>
2018-06-26 12:00:40 +0800

    1111
    11111

---
+++ Assets/Script/GameCenter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameCenter : MonoBehaviour
+{
+    #region Inspecter Variables
+    public GameObject HomeSet;
+
+    #endregion
+    [HideInInspector] public int GamePoint = 0;
+    bool isGameActive = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void PrePareView()
+    {
+
+
+
+    }
+
+}
